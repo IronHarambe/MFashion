13c7b2bf1c62bce7730b7e449c94b52b60738d3d
IronHarambe <49575350+IronHarambe@users.noreply.github.com>
2020-09-07 14:04:56 +1000

    Updates

---
+++ precache-manifest.c2dfa0f0e3cd02553287708c097438b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13745b0c1b286e55ca29e13914276669",
+    "url": "/MFashion/index.html"
+  },
+  {
+    "revision": "09435b387c290d530c4b",
+    "url": "/MFashion/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9393a7d39f89b1549948",
+    "url": "/MFashion/static/js/2.df5f2013.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MFashion/static/js/2.df5f2013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09435b387c290d530c4b",
+    "url": "/MFashion/static/js/main.68a28959.chunk.js"
+  },
+  {
+    "revision": "b8ec1e74285542e40ad1",
+    "url": "/MFashion/static/js/runtime-main.03ce4859.js"
+  }
+]);
